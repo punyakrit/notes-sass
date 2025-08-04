cc3a1b6dd10bc54e97fa9662ae9e858664398557
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-08-05 01:33:42 +0530

    settings page

---
+++ app/dashboard/settings/page.tsx
@@ -0,0 +1,113 @@
+import React from "react";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { prisma } from "@/lib/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+async function getData({id}: {id: string}) {
+  const respsone = await prisma.user.findUnique({
+    where:{
+      id : id
+    },
+    select:{
+      name:true,
+      email:true,
+      colorScheme:true
+    }
+  })
+  return respsone;
+}
+
+
+async function page() {
+
+  const { getUser } = getKindeServerSession()
+  const user = await getUser();
+  const data = await getData({id: user?.id as string});
+
+  return (
+    <div className="grid items-start gap-8">
+      <div className="flex items-center justify-between px-2">
+        <div className="grid gap-1">
+          <h1 className="text-3xl md:text-4xl font-semibold">Settings</h1>
+          <p className="text-lg text-muted-foreground">Your Profile Settings</p>
+        </div>
+      </div>
+      <Card>
+        <form>
+          <CardHeader>
+            <CardTitle>General Data</CardTitle>
+            <CardDescription>
+              Update your profile information and preferences.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="space-y-2 mt-4">
+              <div className="space-y-1">
+                <Label className="text-sm font-medium">Your Name</Label>
+                <Input
+                  className=""
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Your name"
+                ></Input>
+              </div>
+              <div className="space-y-1">
+                <Label className="text-sm font-medium">Your Email</Label>
+                <Input
+                  className=""
+                  type="text"
+                  name="email"
+                  id="email"
+                  placeholder="Your email"
+                  disabled
+                ></Input>
+              </div>
+              <div className="space-y-1">
+                <Label className="text-sm font-medium">Colour Scheme</Label>
+                <Select name="color">
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Select a color scheme" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>Color</SelectLabel>
+                      <SelectItem value="theme-green">Green</SelectItem>
+                      <SelectItem value="theme-blue">Blue</SelectItem>
+                      <SelectItem value="theme-oragne">Orange</SelectItem>
+                      <SelectItem value="theme-red">Red</SelectItem>
+                      <SelectItem value="theme-rose">Rose</SelectItem>
+                      <SelectItem value="theme-purple">Purple</SelectItem>
+                      <SelectItem value="theme-yellow">Yellow</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+          </CardContent>
+        </form>
+      </Card>
+    </div>
+  );
+}
+
+export default page;
